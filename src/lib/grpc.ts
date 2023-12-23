f963e34ae437b9db8559cb8c1b440a75109b0f64
Andre Miras <andre.miras@gmail.com>
2023-12-23 02:10:32 +0100

    :sparkles: Basic gRPC integration
    Fetch last observed Eth nonce and last validator set using gRPC.

---
+++ src/lib/grpc.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import { promisify } from "util";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+
+const gravityQueryProto = "gravity/v1/query.proto";
+const gravityProtoDir = "./src/proto";
+const osmosisProtoDir = "./node_modules/@protobufs/";
+
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+  includeDirs: [gravityProtoDir, osmosisProtoDir],
+};
+
+const getClient = (url: string) => {
+  const packageDefinition = protoLoader.loadSync(gravityQueryProto, options);
+  const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+  const gravityService = protoDescriptor.gravity as any;
+  const client = new gravityService.v1.Query(
+    url,
+    grpc.credentials.createInsecure(),
+  );
+  return client;
+};
+
+const getLastObservedEthNonce = (client: any) =>
+  promisify(client.GetLastObservedEthNonce).bind(client);
+
+const lastValsetRequests = (client: any) =>
+  promisify(client.LastValsetRequests).bind(client);
+
+export { getClient, getLastObservedEthNonce, lastValsetRequests };
