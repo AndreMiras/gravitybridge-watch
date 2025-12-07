852800afc7704d53c7a417b0b3419e733a02dad9
Andre Miras <andre.miras@gmail.com>
2025-12-07 23:14:40 +0000

    :arrow_up: Upgrade Next.js from 14.2.32 to 15.5.7
    Move outputFileTracingIncludes from experimental to top-level
    config as required by Next.js 15.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingIncludes: {
-      "/*": ["./src/proto/**", "./node_modules/@protobufs/**"],
-    },
+  outputFileTracingIncludes: {
+    "/*": ["./src/proto/**", "./node_modules/@protobufs/**"],
   },
   async rewrites() {
     return [
